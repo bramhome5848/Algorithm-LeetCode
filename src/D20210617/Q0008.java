00189411d8ed00e19125603559dc08f1219d8f6d
이경진 <bramhome5848@gmail.com>
2021-06-17 12:03:05 +0900

    20210617 Algorithm Study

--- src/D20210617/Q0008.java
+++ src/D20210617/Q0008.java
@@ -1,11 +1,7 @@
 package D20210617;
 
 public class Q0008 {
-    public static void main(String[] args) {
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(myAtoi("2147483857"));
-    }
+
     public static int myAtoi(String s) {
 
         s = s.trim();
