1bfab61d60bfaf54a50f77ce9a6c58aa9c82a7d8
이경진 <bramhome5848@gmail.com>
2021-09-16 14:22:35 +0900

    20210916 Algorithm Study

---
+++ src/D20210916/Q0442.java
@@ -0,0 +1,25 @@
+package D20210916;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 442. Find All Duplicates in an Array
+ */
+public class Q0442 {
+
+    //must write an algorithm that runs in O(n) time and uses only constant extra space.
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for(int num : nums) {
+            int idx = Math.abs(num) - 1;
+            if (nums[idx] < 0) result.add(idx + 1);
+            nums[idx] = -nums[idx];
+        }
+
+        return result;
+    }
+}
